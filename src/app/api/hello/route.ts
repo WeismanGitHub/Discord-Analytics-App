1da88c526e1352c84408f5ff02c7e2f3143d17d0
weisman <weisman123@outlook.com>
2024-07-16 12:18:01 -0700

    Format

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,9 +1,5 @@
 import { NextResponse } from 'next/server';
 
-type ResponseData = {
-    message: string;
-};
-
 export async function GET() {
     return NextResponse.json({ message: 'hello world' });
 }
