ecea096d06e9a3b419b93f05176fa69cc3acb9f2
weisman <weisman123@outlook.com>
2024-07-17 15:03:01 -0700

    Add database vars to env

--- src/env.ts
+++ src/env.ts
@@ -2,8 +2,20 @@ import zod from 'zod';
 
 const envSchema = zod.object({
     TOKEN: zod.string(),
+    BOT_ID: zod.string(),
+
+    DATABASE: zod.string(),
+    HOST: zod.string(),
+    USER: zod.string(),
+    PORT: zod.number(),
 });
 
 export default envSchema.parse({
     TOKEN: process.env.TOKEN,
+    BOT_ID: process.env.BOT_ID,
+
+    DATABASE: process.env.DATABASE,
+    HOST: process.env.HOST,
+    USER: process.env.USER,
+    PORT: process.env.POST,
 });
