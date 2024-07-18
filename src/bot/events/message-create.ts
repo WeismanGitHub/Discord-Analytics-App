a72737ce7a04165b188fd233050e19f7bf89ee39
weisman <weisman123@outlook.com>
2024-07-18 04:22:01 -0700

    Start saving messages

--- src/bot/events/message-create.ts
+++ src/bot/events/message-create.ts
@@ -1,10 +1,31 @@
+import { Guild, Message as MessageModel } from '../../database/models';
 import { Events, Message } from 'discord.js';
+import guildCache from '../cache';
 
 const event = {
     name: Events.MessageCreate,
-    execute(message: Message) {
+    async execute(message: Message) {
         if (!message.guild) return;
-        console.log(message.guild);
+
+        let value: boolean | undefined = guildCache.get(message.guild.id);
+
+        if (value === undefined) {
+            const guild = await Guild.findOne({ where: { id: message.guild.id } });
+
+            guildCache.set(message.guild.id, Boolean(guild));
+            value = Boolean(guild);
+        }
+
+        if (!value) return;
+
+        await MessageModel.create({
+            id: message.id,
+            userId: message.author.id,
+            channelId: message.channelId,
+            guildId: message.guild.id,
+            bot: message.author.bot,
+            textLength: message.content.length,
+        });
     },
 };
 
