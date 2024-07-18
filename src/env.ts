bc88f97f36a8a34a3412ce30c0bf9264efbd7b70
weisman <weisman123@outlook.com>
2024-07-17 19:50:07 -0700

    Add PASSWORD var

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,10 @@
 import zod from 'zod';
 
 const envSchema = zod.object({
-    TOKEN: zod.string(),
     BOT_ID: zod.string(),
+    TOKEN: zod.string(),
 
+    PASSWORD: zod.string(),
     DATABASE: zod.string(),
     HOST: zod.string(),
     USER: zod.string(),
@@ -15,6 +16,7 @@ export default envSchema.parse({
     BOT_ID: process.env.BOT_ID,
 
     DATABASE: process.env.DATABASE,
+    PASSWORD: process.env.PASSWORD,
     HOST: process.env.HOST,
     USER: process.env.USER,
     PORT: process.env.POST,
