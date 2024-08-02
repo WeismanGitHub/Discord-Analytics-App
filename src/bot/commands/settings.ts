5b1650206d069ad2dcf757ebf029f75e3c60b1a7
weisman <weisman123@outlook.com>
2024-08-02 02:48:20 -0700

    Finish

--- src/bot/commands/settings.ts
+++ src/bot/commands/settings.ts
@@ -6,25 +6,42 @@ import guildCache from '../cache';
 const command = {
     data: new SlashCommandBuilder()
         .setName('settings')
-        .setDescription('Modify the server settings.')
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .setDescription('Modify or view the server settings.')
         .setDMPermission(false)
         .addBooleanOption((option) =>
             option
                 .setName('track-messages')
-                .setRequired(true)
+                .setRequired(false)
                 .setDescription('Set whether messages should be tracked.')
         ),
     async execute(interaction: ChatInputCommandInteraction) {
         if (!interaction.guild) return;
 
-        const trackMessages = interaction.options.getBoolean('track-messages', true);
+        const trackMessages = interaction.options.getBoolean('track-messages');
+        const changes = trackMessages !== null;
 
-        await Guild.upsert({ id: interaction.guild.id, trackMessages });
-        guildCache.set(interaction.guild.id, { trackMessages: true });
+        if (changes) {
+            if (!interaction.memberPermissions?.has(PermissionFlagsBits.Administrator)) {
+                throw new Error('Only administrators can change the settings.');
+            }
+
+            await Guild.upsert({ id: interaction.guild.id, trackMessages });
+            guildCache.set(interaction.guild.id, { trackMessages });
+
+            return await interaction.reply({
+                embeds: [new InfoEmbed('Settings', `Track Messages: \`${trackMessages}\``)],
+                ephemeral: true,
+            });
+        }
+
+        const guild = await Guild.findOne({ where: { id: interaction.guild.id } });
+
+        if (!guild) {
+            throw new Error("This server hasn't been set up yet.");
+        }
 
         await interaction.reply({
-            embeds: [new InfoEmbed('Settings', `Track Messages: \`${trackMessages}\``)],
+            embeds: [new InfoEmbed('Settings', `Track Messages: \`${guild.trackMessages}\``)],
             ephemeral: true,
         });
     },
